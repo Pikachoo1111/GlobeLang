dd392b38cf6d76e102c8398714802c605ceea927
M3z3 <olorajoel@gmail.com>
2024-08-30 20:44:21 -0600

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,4 @@
-
+document.getElementById('logout-button').addEventListener('click', () => {
+    localStorage.removeItem('user');
+    window.location.href = 'login.html'; // Redirect to login page after logout
+});
